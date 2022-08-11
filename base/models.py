964bc16d46cff85705a50c2de859311e1e503ea3
Ricardo Ariel Estrada Pérez <restradap001@alumno.uaemex.mx>
2022-08-11 17:40:43 -0500

    Basic Database Configuration
    some features:
    -restaurant model
    -restaurant type model

--- base/models.py
+++ base/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class RestaurantType(models.Model):
+    type = models.CharField(max_length=16, unique=True)
+
+    class Meta:
+        ordering = ['-type']
+
+    def __str__(self):
+        return self.type
+
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=64, unique=True)
+    type = models.ForeignKey(RestaurantType, on_delete=models.PROTECT)
+    address = models.CharField(max_length=128)
+    telephone = models.CharField(max_length=16, unique=True)
+
+    class Meta:
+        ordering = ['-type', '-name']
+
+    def __str__(self):
+        return self.name
