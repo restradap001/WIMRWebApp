2cc03cf071fc18253f3df02198023007c050dd51
Ricardo Ariel Estrada Pérez <restradap001@alumno.uaemex.mx>
2022-08-11 17:45:03 -0500

    ViewSets Basic Configuration
    some features added:
    -user viewset
    -restaurant viewset

--- base/views.py
+++ base/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import viewsets, permissions
 
-# Create your views here.
+from base.serializers import UserSerializer, RestaurantSerializer
+from .models import Restaurant
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class RestaurantViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows restaurants to be viewed or edited
+    """
+    queryset = Restaurant.objects.all()
+    serializer_class = RestaurantSerializer
+    permission_classes = [permissions.IsAuthenticated]
