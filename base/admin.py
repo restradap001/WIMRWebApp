269ed7bd97aff1921eced2ea559aef7e6a023604
Ricardo Ariel Estrada Pérez <restradap001@alumno.uaemex.mx>
2022-08-11 17:44:05 -0500

    Administrator Panel Configuration
    some features added:
    -restaurant administrator model
    -restaurant type administrator model
    -searching, filtering and ordering features

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Restaurant, RestaurantType
+
+
+@admin.register(Restaurant)
+class RestaurantAdmin(admin.ModelAdmin):
+    list_display = ('name', 'type', 'address', 'telephone',)
+    search_fields = ('name', 'address', 'telephone')
+    list_filter = ('type',)
+
+
+@admin.register(RestaurantType)
+class RestaurantTypeAdmin(admin.ModelAdmin):
+    list_display = ('type',)
